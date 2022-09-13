40a7aadf7292a801ec89375e46b45a74829df838
GuadalupeFrias <trestrebolesamarillos@gmail.com>
2022-09-13 17:21:54 -0300

    incorporacion de  servicios otros componentes

--- FrontEndPortfolioAngular/src/app/components/footer/footer.component.ts
+++ FrontEndPortfolioAngular/src/app/components/footer/footer.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PortfolioService } from 'src/app/service/portfolio.service';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  datosPorfolio:any;
+  constructor( private portfolioService: PortfolioService) { }
 
   ngOnInit(): void {
+    this.portfolioService.getPortfolioData().subscribe(data=>{
+      console.log(data);
+      this.datosPorfolio=data;
+
+  });
   }
 
 }
--- FrontEndPortfolioAngular/src/app/components/header/header.component.ts
+++ FrontEndPortfolioAngular/src/app/components/header/header.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PortfolioService } from 'src/app/service/portfolio.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  datosPorfolio:any;
+  constructor( private portfolioService: PortfolioService) { }
 
   ngOnInit(): void {
+    this.portfolioService.getPortfolioData().subscribe(data=>{
+      console.log(data);
+      this.datosPorfolio=data;
+
+  });
   }
 
 }
